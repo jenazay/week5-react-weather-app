91c386177c78273c997c613a28e9bb4308f19bae
maryzey <jenalurans@gmail.com>
2023-09-26 22:41:14 +0330

    added the GitHub repository

--- src/App.js
+++ src/App.js
@@ -4,8 +4,27 @@ import Weather from "./weather.js"
 
 function App() {
   return (
-    <div className="App">
-      <Weather defaultCity="New York" />
+    <div>
+      <div className="App">
+        <Weather defaultCity="New York" />
+      </div>
+      <div className="footer text-center">
+        <footer>
+          This project was coded by{" "}
+          <span href="/" target="_blank" rel="noreferrer">
+            Zeinab Zamani
+          </span>{" "}
+          and is{" "}
+          <a
+            href="https://github.com/jenazay/week5-react-weather-app"
+            target="_blank"
+            rel="noreferrer"
+          >
+            open-sourced
+          </a>{" "}
+          on GitHub
+        </footer>
+      </div>
     </div>
   );
 }
